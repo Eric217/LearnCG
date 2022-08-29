7d04e679edf2af0397447c89fae1048e2ee97e4c
Eric <erisl@icloud.com>
2022-08-29 23:43:52 +0800

    组织作业 6

--- 6/main.cpp
+++ 6/main.cpp
@@ -12,8 +12,7 @@
 int main(int argc, char** argv)
 {
     Scene scene(1280, 960);
-
-    MeshTriangle bunny("../models/bunny/bunny.obj");
+    MeshTriangle bunny(BUNNY_DIR);
 
     scene.Add(&bunny);
     scene.Add(std::make_unique<Light>(Vector3f(-20, 70, 20), 1));
